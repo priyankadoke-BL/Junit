9f78f5aa74a3dd6d09c2771781b426dea3e3a87f
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-10-16 09:48:52 +0530

    validation process

---
+++ src/test/java/com/bridgelabz/EmailValidation/EmailVal.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.EmailValidation;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailVal{
+    private boolean expectedResult;
+    private String email2Test;
+
+    public EmailVal(String email, boolean result){
+        this.email2Test = email;
+        this.expectedResult = result;
+    }
+    @Parameterized.Parameters
+    public static Collection input(){
+        return Arrays.asList(new Object[][] { {"abc@yahoo.com",true},
+                {"abc-100@yahoo.com",true},
+                {"abc.100@yahoo.com",true},
+                {"abc111@abc.com",true},
+                {"abc-100@abc.net",true},
+                {"abc.100@abc.com.au",true},
+                {"abc@1.com",true},
+                {"abc@gmail.com.com",true},
+                {"abc+100@gmail.com",true},
+                {"abc",false},
+                {"abc@.com.my",false},
+                {"abc123@gmail.a",false},
+                {"abc123@.com",false},
+                {"abc123@.com.com",false},
+                {".abc@abc.com",false},
+                {"abc()*@gmail.com",false},
+                {"abc@%*.com",false},
+                {"abc...2002@gmail.com",false},
+                {"abc.@gmail.com",false},
+                {"abc@abc@gmail.com",false},
+                {"abc@gmail.com.1a",false}});
+    }
+
+    }
